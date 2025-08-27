60a27ad4db78c820d4c169859c54c458551f55c6
alihossen4 <mdahossen.dev@gmail.com>
2025-08-28 02:53:42 +0600

    added apiSuccess and apiError

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,5 +1,5 @@
 class ApiError extends Error{
-    constructor(statusCode, message="Something went wrong",errors={},stack=""){
+    constructor(statusCode, message="Something went wrong",errors=[],stack=""){
         super(message);
         this.success = false
         this.statusCode = statusCode;
@@ -15,21 +15,27 @@ class ApiError extends Error{
     static badRequest(message="bad request", errors=[]){
         return new ApiError(400, message,errors)
     }
+
     static unauthorized(message="unauthorized",errors=[]){
         return new ApiError(401,message,errors);
     }
+
     static forbidden(message="access denied", errors=[]){
         return new ApiError(403,message,errors);
     }
+
     static serverError(message="server error",errors=[]){
         return new ApiError(500,message,errors);
     }
+
     static notFound(message="notFound", errors=[]){
         return new ApiError(404,message,errors)
     }
+
     static conflict(message="conflict", errors=[]){
         return new ApiError(409, message, errors)
     }
+
     static customError(statusCode,message="something went wrong",errors=[]){
         return new ApiError(statusCode,message,errors)
     }
--- src/utils/apiSuccess.js
+++ src/utils/apiSuccess.js
@@ -5,5 +5,16 @@ class ApiSuccess {
     this.message = message;
     this.data = data ?? {};
   }
+  static success(message="success",data={}){
+    return new ApiSuccess(200,message,data);
+  }
+  
+  static created(message="created",data={}){
+    return new ApiSuccess(201,message,data);
+  }
+
+  static accepted(message="accepted",data={}){
+    return new ApiSuccess(202,message,data);
+  }
 }
 export default ApiSuccess;
